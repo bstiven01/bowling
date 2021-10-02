27411062bf2669a421d3224916fc2f784bff2197
Stiven <b.stiven01@gmail.com>
2021-10-01 23:38:53 -0500

    routing fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Beginning from '@/components/Beginning'
 import FormBeginning from '@/components/FormBeginning'
 import GameBowling from '@/components/GameBowling'
@@ -9,11 +8,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/Beginning',
       name: 'Beginning',
